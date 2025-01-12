4b9f618ee8436e4a86c832412edcf5efc83b1e1d
hananeltassa <hananeltassa15@gmail.com>
2025-01-13 00:50:25 +0200

    Added voice model route

--- mood_detection/urls.py
+++ mood_detection/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from mood_detection.voice_detection.views import VoiceMoodDetectionView
 from mood_detection.text_detection.views import TextMoodDetectionView
 
 urlpatterns = [
     path('detect-text-mood/', TextMoodDetectionView.as_view(), name='detect-text-mood'),
+    path('detect-voice-mood/', VoiceMoodDetectionView.as_view(), name='detect-voice-mood'),
 ]
